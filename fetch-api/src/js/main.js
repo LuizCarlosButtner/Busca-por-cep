b7f9757285a3cd9497c9da2ed8b3c10531fc1104
LuizCarlosButtner <udroglot@gmail.com>
2023-04-19 01:57:57 -0300

    everything is fine with the communication with the fetch, but there are some adjustments 'the activation was only with on('blur') so I put on('keyEnter')' I will do some validations.

--- fetch-api/src/js/main.js
+++ fetch-api/src/js/main.js
@@ -1,20 +1,58 @@
 
-$("#cep").on("blur", (e)=>{
-    let cep = $("#cep").val().replace("-","")
-    const options ={
+
+$("#cep").on("blur", (e) => {
+    chamadaDaApi(e)
+})
+
+$("#cep").on("keypress", (e) => {
+    if (e.which === 13) {
+        chamadaDaApi(e)
+    }
+})
+
+
+
+const chamadaDaApi = (e) => {
+    let cep = $("#cep").val().replace("-", "")
+
+    const options = {
         method: 'GET',
-        mode:'cors',
-        cache:'default'
+        mode: 'cors',
+        cache: 'default'
     }
 
     fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    .then(response=>{response.json()
-        .then(data => console.log(data))
+    .then(response => {
+        response.json()
+            .then(data => {
+                if(data.erro == "true"){
+                    console.log("Cep inexistente")
+                    apagarTudo()
+                } else {
+                    preenche(data) 
+                }
+                console.log(data)
+            }
+            )
     })
-    .catch(e => console.log("Error"))
+    .catch(e => alert("Error!!!"))
+}
 
+const preenche = (param) => {
 
+    $('#logradouro').val(param.logradouro)
+    $('#bairro').val(param.bairro)
+    $('#localidade').val(param.localidade)
+    $('#uf').val(param.uf)
 
-})  
+}
+
+const apagarTudo =_=> {
+    $('#cep').val("")
+    $('#logradouro').val("")
+    $('#bairro').val("")
+    $('#localidade').val("")
+    $('#uf').val("")
+}
 
 
